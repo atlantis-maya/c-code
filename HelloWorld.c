d92fc343031fe529ba69df4f191dfba2b322d677
ZhuHe <zhuhe88@gmail.com>
2017-02-12 17:40:49 +0800

    second time commit about HelloWorld.c and add compile file HelloWorld.c and create hello

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
+void test();
 int main(){
-  printf("this is first program of C language");
-  printf("thinks!!!!");
+  printf("this is first program of C language\n");
+  printf("thinks!!!!\n");
   test();
  
 
 }
 void test(){
- printf("this is test method")
+ printf("this is test method\n");
 }
